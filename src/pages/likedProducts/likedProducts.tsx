8188760ac6b9af463e5b070dd6640622bde82d65
Vladimir Seropol <frontender23@mail.ru>
2024-12-27 11:10:14 +0300

    edit

--- src/pages/likedProducts/likedProducts.tsx
+++ src/pages/likedProducts/likedProducts.tsx
@@ -2,16 +2,26 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../../store/store";
 import ProductCard from "../../components/ProductCard/ProductCard";
+import Button from "../../components/Button/Button";
+import { useNavigate } from "react-router-dom";
 
 const LikedProductsPage: React.FC = () => {
   const likedProducts = useSelector((state: RootState) =>
     state.products.products.filter((product) => product.isLiked)
   );
 
+  const navigate = useNavigate();
+
+  const goBack = () => {
+    navigate("/");
+  };
+
   return (
     <div className="container">
       <h1>Избранные товары</h1>
+       <Button onClick={goBack} children={"На страницу продуктов"} />
       <div className="products-list">
+     
         {likedProducts.map((product) => (
           <ProductCard
             key={product.id}
